191578398a3dea084a3f7545ad697eac92a8fe7f
Brandan Richardson <brandanrichardson@Brandans-MacBook-Pro.local>
2018-01-22 09:10:49 -0800

    commit 1/22/18 9:10AM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,66 @@
-// Variable for random number generator for target score
+// Undefined variable for random number
+var randomNum;
 
-// Variable for crystal1
+// Undefined variable for blue crystal
+var blueCrystal;
 
-// Variable for crystal2
+// Undefined variable for orange crystal
+var orangeCrystal;
 
-// Variable for crystal3
+// Undefined variable for green crystal
+var greenCrystal;
 
-// / Variable for crystal4
+// Undefined variable for red crystal
+var redCrystal;
 
-// Variable for wins
+// Variable for sum of user's guesses
+var yourSum = 0;
 
-// Variable for losses
+// Undefined variable result
+var result;
+
+// Variables for wins and losses counter
+var winsCounter = 0;
+var lossesCounter = 0;
 
 // Variable for reset
+var reset= function() {
+
+};
+
+// Event handler that starts game
+$( document ).ready(function() {
+  		
+		randomNum = [Math.floor(Math.random() * 101) + 19];
+			console.log("Computer chose this: " + randomNum);
+
+			$("#targetscore").text(randomNum);
+
+		yourSum = blueCrystal + orangeCrystal + greenCrystal +redCrystal;
+
+		// On click event to generate random #'s for each crystal
+		$("#blue").on("click", function() {
+		blueCrystal = [Math.floor(Math.random() * 13) + 1];
+			console.log("Blue Crystal's value is: " + blueCrystal);
+			yourSum +=blueCrystal;
+			$("#sum").text(yourSum);
+			if (yourSum === randomNum) {
+				winsCounter ++;
+			}	
+    	})
+    	$("#orange").on("click", function() {
+		orangeCrystal = [Math.floor(Math.random() * 13) + 1];
+			console.log("Orange Crystal's value is: " + orangeCrystal);	
+		})
+    	$("#green").on("click", function() {
+		greenCrystal = [Math.floor(Math.random() * 13) + 1];
+			console.log("Green Crystal's value is: " + greenCrystal);
+		})
+		$("#red").on("click", function() {
+		redCrystal = [Math.floor(Math.random() * 13) + 1];
+			console.log("Red Crystal's value is: " + redCrystal);
+		})
+
+  });
+  	
+// });
