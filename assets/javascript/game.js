042d61fa91060eb4a768d742cd2d8d71a38d367f
Brandan Richardson <brandanrichardson@brandans-mbp.attlocal.net>
2018-01-21 23:02:02 -0800

    commit 1/21/18 11:01PM

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Variable for random number generator for target score
+
+// Variable for crystal1
+
+// Variable for crystal2
+
+// Variable for crystal3
+
+// / Variable for crystal4
+
+// Variable for wins
+
+// Variable for losses
+
+// Variable for reset
