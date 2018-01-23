09b4fe466378d081cbeeb6b2d9e00adbd23ecf93
Brandan Richardson <brandanrichardson@Brandans-MacBook-Pro.local>
2018-01-23 06:40:31 -0800

    commit 1/23/18 6:40AM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,18 @@
+// Variable for secret number array for each crystal
+var secretNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+// Variable for sum of user's guesses
+var yourSum = 0; 
+
+// Variables for wins and losses counter
+var winsCounter = 0;
+var lossesCounter = 0;
+
+// Variable for reset
+var reset= function() {
+
+};
+
 // Undefined variable for random number
 var randomNum;
 
@@ -13,20 +28,10 @@ var greenCrystal;
 // Undefined variable for red crystal
 var redCrystal;
 
-// Variable for sum of user's guesses
-var yourSum = 0;
-
 // Undefined variable result
-var result;
+var imageCrystal;
 
-// Variables for wins and losses counter
-var winsCounter = 0;
-var lossesCounter = 0;
 
-// Variable for reset
-var reset= function() {
-
-};
 
 // Event handler that starts game
 $( document ).ready(function() {
@@ -34,33 +39,79 @@ $( document ).ready(function() {
 		randomNum = [Math.floor(Math.random() * 101) + 19];
 			console.log("Computer chose this: " + randomNum);
 
+		blueCrystal = secretNumber[Math.floor(Math.random() * secretNumber.length)];
+			console.log("Blue Crystal's value is: " + blueCrystal);
+
+		orangeCrystal = secretNumber[Math.floor(Math.random() * secretNumber.length)];
+			console.log("Orange Crystal's value is: " + orangeCrystal);	
+
+		greenCrystal = secretNumber[Math.floor(Math.random() * secretNumber.length)];
+			console.log("Green Crystal's value is: " + greenCrystal);
+
+		redCrystal = secretNumber[Math.floor(Math.random() * secretNumber.length)];
+			console.log("Red Crystal's value is: " + redCrystal);
+
+		var crystalNumbers = [blueCrystal, orangeCrystal, greenCrystal, redCrystal]	
+
 			$("#targetscore").text(randomNum);
 
-		yourSum = blueCrystal + orangeCrystal + greenCrystal +redCrystal;
+			for (var i = 0; i < crystalNumbers.length; i++) {
+
+				imageCrystal = $("#crystals img");
+
+				imageCrystal.addClass("crystal-image");
+
+				// imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+				imageCrystal.attr("data-crystalvalue", crystalNumbers[i]);
+				console.log(crystalNumbers[i]);
+				
+
+				$(".crystal-image").append(imageCrystal);
+  }
+
+
+
+			// For each iteration, we will create an imageCrystal
+			
+
+			// Each imageCrystal will be given a data attribute called data-crystalValue.
+    		// This data attribute will be set equal to the array value.
+    		
+
+		// yourSum = blueCrystal + orangeCrystal + greenCrystal +redCrystal;
 
 		// On click event to generate random #'s for each crystal
-		$("#blue").on("click", function() {
-		blueCrystal = [Math.floor(Math.random() * 13) + 1];
-			console.log("Blue Crystal's value is: " + blueCrystal);
-			yourSum +=blueCrystal;
-			$("#sum").text(yourSum);
-			if (yourSum === randomNum) {
-				winsCounter ++;
-			}	
-    	})
-    	$("#orange").on("click", function() {
-		orangeCrystal = [Math.floor(Math.random() * 13) + 1];
-			console.log("Orange Crystal's value is: " + orangeCrystal);	
-		})
-    	$("#green").on("click", function() {
-		greenCrystal = [Math.floor(Math.random() * 13) + 1];
-			console.log("Green Crystal's value is: " + greenCrystal);
-		})
-		$("#red").on("click", function() {
-		redCrystal = [Math.floor(Math.random() * 13) + 1];
-			console.log("Red Crystal's value is: " + redCrystal);
-		})
+		$(".crystal-image").on("click", function() {
+			
+				var crystalValue = ($(this).attr("data-crystalvalue"));
+    			crystalValue = parseInt(crystalValue);
 
-  });
+
+				yourSum +=crystalValue;
+				$("#sum").text(yourSum);
+				
+				if (yourSum == randomNum) {
+				winsCounter ++;
+				$("#wins").text(winsCounter);
+				}
+				else if (yourSum >= randomNum) {
+     			lossesCounter ++;
+     			$("#losses").text(lossesCounter);
+    }
+			})	
+    	});
+
+  //   	$("#orange").on("click", function() {
+		// })
+  //   	$("#green").on("click", function() {
+		// 
+		// 
+		// })
+		// $("#red").on("click", function() {
+		// 
+		// })
+
+  // });
   	
 // });
